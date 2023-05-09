12c8cfd056fda1e208ff39ac62a33d5e04a16c91
David Leksen <dleksen@protonmail.com>
2023-05-09 08:25:40 -0400

    Upgrade to latest hjson-cpp

--- Source/d2Hjson.cpp
+++ Source/d2Hjson.cpp
@@ -21,43 +21,46 @@ namespace d2d
 	}
 	bool IsNonNull(const HjsonValue& data)
 	{
-		return (data.type() != HjsonValue::UNDEFINED && data.type() != HjsonValue::HJSON_NULL);
+		return (data.type() != Hjson::Type::Undefined && data.type() != Hjson::Type::Null);
 	}
 	HjsonValue GetMemberValue(const HjsonValue& data, const std::string& name)
 	{
-		if(data.type() == HjsonValue::MAP && IsNonNull(data[name]))
+		if(data.type() == Hjson::Type::Map && IsNonNull(data[name]))
 			return data[name];
 		throw HjsonFailedQueryException{ name };
 	}
 	std::string GetString(const HjsonValue& data, const std::string& name)
 	{
 		HjsonValue v{ GetMemberValue(data, name) };
-		if(v.type() == HjsonValue::STRING)
+		if(v.type() == Hjson::Type::String)
 			return v;
 		throw HjsonFailedQueryException{ name };
 	}
 	float GetFloat(const HjsonValue& data, const std::string& name)
 	{
 		HjsonValue v{ GetMemberValue(data, name) };
-		if(v.type() == HjsonValue::DOUBLE)
+		if(v.type() == Hjson::Type::Double)
 			return static_cast<float>(v);
 		throw HjsonFailedQueryException{ name };
 	}
 	double GetDouble(const HjsonValue& data, const std::string& name)
 	{
 		HjsonValue v{ GetMemberValue(data, name) };
-		if(v.type() == HjsonValue::DOUBLE)
+		if(v.type() == Hjson::Type::Double)
 			return static_cast<double>(v);
 		throw HjsonFailedQueryException{ name };
 	}
 	int GetInt(const HjsonValue& data, const std::string& name)
 	{
-		return GetRoundedDouble(GetDouble(data, name));
+		HjsonValue v{ GetMemberValue(data, name) };
+		if(v.type() == Hjson::Type::Int64)
+			return static_cast<int>(v);
+		throw HjsonFailedQueryException{ name };
 	}
 	bool GetBool(const HjsonValue& data, const std::string& name)
 	{
 		HjsonValue v{ GetMemberValue(data, name) };
-		if(v.type() == HjsonValue::BOOL)
+		if(v.type() == Hjson::Type::Bool)
 			return static_cast<bool>(v);
 		throw HjsonFailedQueryException{ name };
 	}
@@ -65,45 +68,49 @@ namespace d2d
 	HjsonValue GetVectorValue(const HjsonValue& data, const std::string& name, unsigned index)
 	{
 		HjsonValue v{ GetMemberValue(data, name) };
-		if(v.type() == HjsonValue::VECTOR && index < v.size())
+		if(v.type() == Hjson::Type::Vector && index < v.size())
 			return v[index];
 		throw HjsonFailedQueryException{ name };
 	}
 	std::string GetVectorString(const HjsonValue& data, const std::string& name, unsigned index)
 	{
 		HjsonValue v{ GetVectorValue(data, name, index) };
-		if(v.type() == HjsonValue::STRING)
+		if(v.type() == Hjson::Type::String)
 			return v;
 		throw HjsonFailedQueryException{ name };
 	}
 	float GetVectorFloat(const HjsonValue& data, const std::string& name, unsigned index)
 	{
 		HjsonValue v{ GetVectorValue(data, name, index) };
-		if(v.type() == HjsonValue::DOUBLE)
+		if(v.type() == Hjson::Type::Double)
 			return static_cast<float>(v);
 		throw HjsonFailedQueryException{ name };
 	}
 	double GetVectorDouble(const HjsonValue& data, const std::string& name, unsigned index)
 	{
 		HjsonValue v{ GetVectorValue(data, name, index) };
-		if(v.type() == HjsonValue::DOUBLE)
+		if(v.type() == Hjson::Type::Double)
 			return static_cast<double>(v);
 		throw HjsonFailedQueryException{ name };
 	}
 	int GetVectorInt(const HjsonValue& data, const std::string& name, unsigned index)
 	{
-		return GetRoundedDouble(GetVectorDouble(data, name, index));
+		HjsonValue v{ GetVectorValue(data, name, index) };
+		if(v.type() == Hjson::Type::Int64)
+			return static_cast<int>(v);
+		throw HjsonFailedQueryException{ name };
+
 	}
 	bool GetVectorBool(const HjsonValue& data, const std::string& name, unsigned index)
 	{
 		HjsonValue v{ GetVectorValue(data, name, index) };
-		if(v.type() == HjsonValue::BOOL)
-			return v;
+		if(v.type() == Hjson::Type::Bool)
+			return (bool)v;
 		throw HjsonFailedQueryException{ name };
 	}
 	Color GetColorFloat(const HjsonValue& data, const std::string& name)
 	{
-		return Color{ 
+		return Color{
 			GetVectorFloat(data, name, 0),
 			GetVectorFloat(data, name, 1),
 			GetVectorFloat(data, name, 2),
@@ -111,7 +118,7 @@ namespace d2d
 	}
 	Color GetColorInt(const HjsonValue& data, const std::string& name)
 	{
-		return Color{ 
+		return Color{
 			GetVectorInt(data, name, 0),
 			GetVectorInt(data, name, 1),
 			GetVectorInt(data, name, 2),
